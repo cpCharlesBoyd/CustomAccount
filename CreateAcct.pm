59e3280a8ff0833b03da94cc3afcfb54bf1523ae
Charles Boyd <charles.boyd@cpanel.net>
2012-10-08 11:25:09 -0500

    Modified hook for POSIX compliance

--- CreateAcct.pm
+++ CreateAcct.pm
@@ -23,17 +23,13 @@ sub get_unix_user {
     my ( $context, $data ) = @_;
     
     my $cpanel_user = $data->{'user'};
-    
-    open(FH, "<", "/etc/passwd") or die "open failed: $!";
-    
-    while (my $line = <FH>) {
-	my ( $user, $pass, $uid, $gid, $group, $home, $shell ) = split(":", $line);
-	
-	if ( $user eq $cpanel_user ) {
-	    print "UID and GID for new user $user:", "\n";
-	    print "uid = $uid", "\n";
-	    print "gid = $gid", "\n";
-	}
+      
+    my ( $user, $pass, $uid, $gid, $quota, $comment, $group, $home, $shell ) = getpwnam($cpanel_user);
+
+    if ( $user eq $cpanel_user ) {
+	print "UID and GID for new user $user:", "\n";
+	print "uid = $uid", "\n";
+	print "gid = $gid", "\n";
     }
 }
 
